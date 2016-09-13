3807294d25701de755287f83c6aa8509c56d13b5
mnaumoski <naumoski.marija@gmail.com>
2016-09-13 13:47:47 -0400

    update in app.js with success in AJAX call and working APis

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,7 @@
 // setup variables
 // ==============================
 var queryTerm = "";
-var numResults = 0;
+var numRecords = 0;
 var startYear = 0;
 var endYear = 0;
 
@@ -15,23 +15,60 @@ var articleCounter = 0;
 // ==============================
 
 function runQuery(numArticles, queryURL) {
-  // AJAX FUnction
-  $.ajax({url: queryURL, type: 'GET', })
-    .done(function(NYTData) {
-      // logging to console
-      console.log(NYTData);
-    });
+    // AJAX FUnction
+    $.ajax({ url: queryURL, type: 'GET', })
+        .done(function(NYTData) {
+            for (var i = 0; i < numArticles; i++) {
+                // logging to console
+                console.log("===Headline===");
+                console.log(NYTData.response.docs[i].headline.main);
+                console.log(NYTData.response.docs[i].web_url);
+                console.log(NYTData.response.docs[i].byline.original);
+                console.log(NYTData.response.docs[i].section_name);
+                console.log(NYTData.response.docs[i].pub_date);
+            }
+
+
+            // logging to console
+            console.log(numArticles);
+            console.log(NYTData);
+            console.log(queryURL);
+        });
 }
 
 // main processes
 // ==============================
 $("#searchBtn").on('click', function() {
-    runQuery(10, "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=e0130c2a061f49eca6cca1992cc2d736&q=obama&begin_date=19990101&end_date=20100101");
+    // add search term
+    var queryTerm = $("#search").val().trim();
+    // the new URL includes the search term
+    var newURL = queryURLBase + "&q=" + queryTerm;
+    // add the number of records to retrieve
+    var numRecords = $("#numRecords").val();
+    // add start date
+    startyear = $("#startYear").val().trim();
+    // add end date
+    endYear = $("#endYear").val().trim();
+
+    if (parseInt(startYear)) {
+        startyear = $("#startYear").val().trim() + "0101";
+        // include start date into the url
+        newURL = newURL + "&begin_date=" + startYear;
+    }
+
+    if (parseInt(endYear)) {
+        endYear = $("#endYear").val().trim() + "0101";
+        // include end date into the url
+        newURL = newURL + "&end_year=" + endYear;
+    }
+
+    runQuery(numRecords, newURL);
+
     return false;
-  });
-  // 1. Capture user input and convert to variables
-  // 2. Run AJAX call to NYTimes
-  // 3. Break down the object response into usable fields
-  // 4. Dynamically generate html
+});
+// 1. Capture user input and convert to variables
+// 2. Run AJAX call to NYTimes
+// 3. Break down the object response into usable fields
+// 4. Dynamically generate html
 
 // 5. Deal with edge cases - bugs or something unexpected
